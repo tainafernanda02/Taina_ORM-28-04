ce280cab261b05e0bb7b09ca07f6f5b28ffd86da
Taina Souza <86112244+tainafernanda02@users.noreply.github.com>
2022-04-27 21:20:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+var { usuario } = require("./models");
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}))
+app.get("/",function(req, res){
+  res.send("T-T")
+});
+
+app.post("/",function(req, res){
+  var resultado = usuario.create(req.body);
+  res.json(resultado);
+});
+
+app.listen(3000, function(){
+  console.log("Eba")
+});
